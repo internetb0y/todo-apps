ac32b9d4c1116cc8f0cdc6dc952cd8babcf6c3a9
Aji Wicaksana <1.internetboy.0@gmail.com>
2024-06-30 22:14:17 +0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,19 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
-    mode: 'none',
+    mode: 'development',
     entry: './src/scripts/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Todo Apps',
+            template: './src/template/index.html',
+            filename: 'index.html'
+        })
+    ]
 }
 
 module.exports = config
